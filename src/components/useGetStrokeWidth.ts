b9733cc3550cca143dca848e233585eb24936cdc
Martin Tasker <martin.tasker@databatix.com>
2024-06-18 06:48:54 +0100

    xt-intro: worldlines for Alice, Charlie and their missiles

--- src/components/useGetStrokeWidth.ts
+++ src/components/useGetStrokeWidth.ts
@@ -6,11 +6,11 @@ export default function useGetStrokeWidth(w: 0 | 1 | -1): number {
   return useMemo(() => {
     switch (w) {
       case 1:
-        return unitStrokeWidth * 2;
+        return unitStrokeWidth * 3;
       case -1:
         return unitStrokeWidth * 0.25;
       case 0:
         return unitStrokeWidth;
     }
-  }, [w]);
+  }, [unitStrokeWidth, w]);
 }
--- src/pages/XTIntroPage.tsx
+++ src/pages/XTIntroPage.tsx
@@ -2,6 +2,7 @@ import React, {ReactElement} from 'react';
 import UFlexbox from '../ui/UFlexbox';
 import {URFigure} from '../components/URFigure';
 import {XTGrid} from '../components/XTGrid';
+import {URLine} from '../components/URLine';
 
 export default function TestGridsPage(): ReactElement {
   return (
@@ -11,7 +12,10 @@ export default function TestGridsPage(): ReactElement {
         <UFlexbox direction="column">
           <URFigure pixelSize={[800, 600]} xRange={[-1, 9]} yRange={[-1, 9]}>
             <XTGrid xRange={[0, 8]} tRange={[0, 8]} />
-            <XTGrid xRange={[0, 8]} tRange={[0, 8]} v={0.5} />
+            <URLine p1={[0, 0]} p2={[4, 8]} strokeWidth={1} />
+            <URLine p1={[4, 0]} p2={[8, 8]} strokeWidth={1} />
+            <URLine p1={[1, 2]} p2={[7, 6]} />
+            <URLine p1={[5, 2]} p2={[3, 6]} />
           </URFigure>
         </UFlexbox>
       </UFlexbox>
